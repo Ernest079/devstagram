63ff732db4e869f8fcde6f3f0d0ecf7c8afc4a08
Ernest079 <ernestomendoza.martinez73@gmail.com>
2025-01-22 13:56:49 -0600

    Editar Perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Imagick\Driver;
 
 class PerfilController extends Controller
 {
@@ -18,7 +21,27 @@ class PerfilController extends Controller
         $request->request->add(['username' => Str::slug($request->username),]);
 
         $request->validate([
-            'username' => 'required|unique:users|min:3|max:20',
+            'username' => ['required', 'unique:users,username,'.auth()->user()->id,'min:3', 'max:20', 'not_in:twitter,editar-perfil'],
         ]);
+
+        if($request->imagen){
+            $imagen = $request->file('imagen');
+
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+            $manager = new ImageManager(new Driver());
+            $imagenServidor = $manager::imagick()->read($imagen);
+            $imagenServidor->resizeDown(1000, 1000);
+
+            $imagenPath = public_path('perfiles') . '/' . $nombreImagen;
+            $imagenServidor->save($imagenPath);
+        }
+
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request ->username;
+        $usuario->imagen = $nombreImagen ?? auth()->user()->imagen ?? '';
+        $usuario->save();
+
+        return redirect()->route('post.index', $usuario->username);
     }
 }
